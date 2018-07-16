ad022d8102b8a7746e336adc491dafa280eeebe6
cmcorrales <cmcorrales3@gmail.com>
2018-07-15 22:36:04 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,69 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  count = 1
+  songs.each do |song|
+    puts "#{count}. #{song}"
+    count += 1
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  response = gets.chomp
+  range = (1..10)
+  response_int = response.to_i
+  if range.include?(response_int)
+    puts "Playing #{songs[response_int - 1]}"
+  elsif songs.include?(response)
+    puts "Playing #{response}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox()
+  puts "Goodbye"
+end
+
+def run
+  songs = [
+      "Phoenix - 1901",
+      "Tokyo Police Club - Wait Up",
+      "Sufjan Stevens - Too Much",
+      "The Naked and the Famous - Young Blood",
+      "(Far From) Home - Tiga",
+      "The Cults - Abducted",
+      "Phoenix - Consolation Prizes",
+      "Harry Chapin - Cats in the Cradle",
+      "Amos Lee - Keep It Loose, Keep It Tight"
+    ]
+  help
+  loop do 
+    puts "Please enter a command:"
+    command = gets.chomp
+    if command == "list"
+      list(songs)
+    elsif command == "play"
+      play(songs)
+    elsif command == "help"
+      help()
+    elsif command == "exit"
+      exit_jukebox()
+      break
+    end
+  end
+end
+
+run
+
+
